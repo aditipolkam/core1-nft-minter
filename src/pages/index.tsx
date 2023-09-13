342c1bb1eb247853eed139a051d89f648ca2c964
Aditi Polkam <aditipolkam@gmail.com>
2023-09-13 11:35:18 +0530

    add connected component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import NavBar from "@/components/Navbar"
 import Disconnected from "@/components/Disconnected"
+import { useWallet } from "@solana/wallet-adapter-react"
+import Connected from "@/components/Connected"
 
 const Home: NextPage = () => {
-
+  const { connected } = useWallet()
+  
   return (
     <div className={styles.container}>
       <Head>
